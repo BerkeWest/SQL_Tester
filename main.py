11e6e58307e16435d33e5bf0ada520b1e3feab3b
BerkeWest <109593509+BerkeWest@users.noreply.github.com>
2022-08-10 13:34:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 import warnings
-import iqr2
-import add_dataframe2
+import iqr
+import add_dataframe
 import sqlite3
 
 warnings.filterwarnings("ignore")
 df=pd.read_csv('.\\deneme.txt',sep="|")
-iqr2.IQR_filter(df)
+iqr.IQR_filter(df)
 
 con = sqlite3.connect('inventorylist.db')
 df.to_sql(name="inventory",con=con,if_exists= "replace", index=False)
@@ -16,6 +16,6 @@ con.commit()
 xlWriter = pd.ExcelWriter("inventorylist.xlsx",engine='xlsxwriter')
 workbook  = xlWriter.book
 
-add_dataframe2.addframe(df,xlWriter)
+add_dataframe.addframe(df,xlWriter)
 workbook.close()
 print(df)
